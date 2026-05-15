7a11ad19bd93316545d081662c67863d69756509
Agent005-dev <huseynsemedzade0@gmail.com>
2026-05-15 19:53:46 +0400

    solution

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 
 a = int(input())
 say = 0
-for i in range(2,a//2+1):
-    if a % i == 0:
-         say+=1
-         print('sade deyil')
+if a != 1:
+     for i in range(2,a//2+1):
+       if a % i == 0:
+            say+=1
+if say == 0:
+             print('sadedir')
+else:
+           print('sade deyil ')
     
-if say==0:
-     print("sadedir")
+    
